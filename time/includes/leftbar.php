b7e4527cc1d90c756132124da4a622a8aa6c0545
osetrovmixail <mixail.osetrov.02@mail.ru>
2022-11-24 13:03:43 +0300

    test
    test

---
+++ time/includes/leftbar.php
@@ -0,0 +1,14 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Главная</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> календарь</a></li>
+			
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i>  Выроботка</a></li>
+	<li><a href="reg-users.php"><i class="fa fa-users"></i> сотрудники</a></li>
+		
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Изменить контактную информацию</a></li>
+
+		
+			</ul>
+		</nav>
